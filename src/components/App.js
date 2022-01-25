4036c8400da3aa2154a6179cea74965043157d3d
radu200 <raduprodan200@gmail.com>
2022-01-25 21:20:48 +0000

    added getCharactersfunction that loads  asynchronously  the character list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import logo from "../logo.svg";
 import styles from "./App.module.scss";
+import * as API from "../api/characters";
 
 // Character list is available in the public directory
 
 export default function App() {
+  const [state, setState] = useState({
+    characters: [],
+    searchCategories: [],
+    spinner: false,
+    err: "",
+  });
+
+  const getCharacters = useCallback(async () => {
+    try {
+      setState((state) => ({ ...state, spinner: true }));
+      const res = await API.getCharacters();
+      if (res.status === 200) {
+        setState((state) => ({
+          ...state,
+          permanentCharacters: res.data,
+          characters: res.data,
+        }));
+      }
+    } catch (err) {
+      setState((state) => ({
+        ...state,
+        err: "An error occured",
+      }));
+    } finally {
+      setState((state) => ({ ...state, spinner: false }));
+    }
+  }, []);
+
+  useEffect(() => {
+    getCharacters();
+  }, [getCharacters]);
+
   return (
     <div className={styles.App}>
       <header className={styles["App-header"]}>
